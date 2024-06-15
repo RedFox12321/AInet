f401ce1cacb29227cf5e2d1273294895af160928
SimaoAntunes <99098141+SimaoAntunes26@users.noreply.github.com>
2024-06-15 12:35:26 +0100

    Better screenings button

--- resources/views/main/movies/show.blade.php
+++ resources/views/main/movies/show.blade.php
@@ -4,42 +4,46 @@
 
 @section('main')
 
-<div class="flex justify-center p-8 bg-stone-700">
-    <div class="w-full max-w-5xl grid grid-cols-2 grid-rows-2 gap-8">
-        <!-- Movie Poster Top -->
-        <div class="col-span-1 row-span-2 w-full h-full relative">
-            <div class="w-full h-full absolute bg-white opacity-90 border-8 border-stone-300">
-                <figure class="h-full w-full flex flex-col">
-                    <a class="h-full w-full flex items-center justify-center" href="{{ route('movies.show', ['movie' => $movie]) }}">
-                        <img class="h-full w-auto" src="{{ $movie->imageUrl }}" alt="{{ $movie->title }}">
-                    </a>
-                </figure>
+    <div class="flex justify-center p-8 bg-stone-700">
+        <div class="w-full max-w-5xl grid grid-cols-2 grid-rows-2 gap-8">
+            <!-- Movie Poster Top -->
+            <div class="col-span-1 row-span-2 w-full h-full relative">
+                <div class="w-full h-full absolute bg-white opacity-90 border-8 border-stone-300">
+                    <figure class="h-full w-full flex flex-col">
+                        <a class="h-full w-full flex items-center justify-center"
+                            href="{{ route('movies.show', ['movie' => $movie]) }}">
+                            <img class="h-full w-auto" src="{{ $movie->imageUrl }}" alt="{{ $movie->title }}">
+                        </a>
+                    </figure>
+                </div>
             </div>
-        </div>
 
-        <!-- Movie Details -->
-        <div class="bg-stone-900 p-8 rounded-[45px] border-2 border-rose-900 shadow-lg">
-            <h1 class="text-3xl font-bold mb-2">{{ $movie->title }}</h1>
-            <p class="mb-4 text-sm text-stone-600 dark:text-stone-300">Year: {{ $movie->year }} &nbsp;&nbsp; Genre: {{ $movie->genre->name }}</p>
-            <p class="mb-4 text-sm text-stone-600 border-t-2 border-rose-900 pt-4 dark:text-stone-300 text-justify">{{ $movie->synopsis }}</p>
+            <!-- Movie Details -->
+            <div class="bg-stone-900 p-8 rounded-[45px] border-2 border-rose-900 shadow-lg">
+                <h1 class="text-3xl font-bold mb-2">{{ $movie->title }}</h1>
+                <p class="mb-4 text-sm text-stone-600 dark:text-stone-300">Year: {{ $movie->year }} &nbsp;&nbsp; Genre:
+                    {{ $movie->genre->name }}</p>
+                <p class="mb-4 text-sm text-stone-600 border-t-2 border-rose-900 pt-4 dark:text-stone-300 text-justify">
+                    {{ $movie->synopsis }}</p>
 
-            <!-- Button -->
-            <div class="mt-8 flex justify-center">
-                <a href="{{ route('screenings.index', ['search' => $movie->title]) }}" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">
-                    See all sessions
-                </a>
+                <!-- Button -->
+                <div class="mt-8 flex justify-center">
+                    <x-button-round>
+                        <a href="{{ route('screenings.index', ['search' => $movie->title]) }}">
+                            See all sessions
+                        </a>
+                    </x-button-round>
+                </div>
             </div>
-        </div>
 
-        <!-- Showtimes -->
-        <div class="bg-stone-900 p-8 rounded-[45px] border-2 border-rose-900 shadow-lg">
-            <h2 class="text-2xl font-semibold mb-4">Next Sessions:</h2>
-            <ul class="list-disc list-inside">
-                <x-screenings.table
-                    :screenings="$screenings"/>
-            </ul>
+            <!-- Showtimes -->
+            <div class="bg-stone-900 p-8 rounded-[45px] border-2 border-rose-900 shadow-lg">
+                <h2 class="text-2xl font-semibold mb-4">Next Sessions:</h2>
+                <ul class="list-disc list-inside">
+                    <x-screenings.table :screenings="$screenings" />
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
