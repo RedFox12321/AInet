95088785e3924328ce7eb3a6b53697fb15d555fc
Gameiro <alexandrefgame@gmail.com>
2024-06-17 11:37:35 +0100

    screening table better

--- resources/views/main/screenings/index.blade.php
+++ resources/views/main/screenings/index.blade.php
@@ -7,32 +7,19 @@
     <div class="flex flex-wrap justify-around">
         <div class="h-max w-max flex fit-content justify-left m-10">
 
-            <x-screenings.filter-card
-            :filterAction="route('screenings.index')"
-            :resetUrl="route('screenings.index')"
-            :title="old('search',$filterByTitleSynopsis)"
-            :theater="old('theater', $filterByTheater)"
-            :genre="old('genre',$filterByGenre)"
-            :date="old('date',$filterByDate)"
-            :genres="$genres->pluck('name','code')->toArray()"
-            :theaters="$theaters->pluck('name', 'name')->toArray()"
-            />
-                
-            {{-- <x-fields.select name="genre" label="Genre"
-                         value=App\Models\Genre
-                        :options="$listGenre"/> --}}
-
+            <x-screenings.filter-card :filterAction="route('screenings.index')" :resetUrl="route('screenings.index')" :title="old('search', $filterByTitleSynopsis)" :theater="old('theater', $filterByTheater)"
+                :genre="old('genre', $filterByGenre)" :date="old('date', $filterByDate)" :genres="$genres->pluck('name', 'code')->toArray()" :theaters="$theaters->pluck('name', 'name')->toArray()" />
         </div>
 
         <div class="flex flex-col">
             <div class="m-10 flex fit-content">
-                <x-screenings.table :screenings="$screenings"/>
+                <x-screenings.table :screenings="$screenings" />
             </div>
             <div>
                 {{ $screenings->links() }}
             </div>
         </div>
-        
+
     </div>
 
 @endsection
